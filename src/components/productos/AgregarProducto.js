dc879d9ba9b184d3bc5d2b7a25ce52372e6d9f73
Emilse <emilsearias@gmail.com>
2021-11-25 10:55:17 -0300

    se agrego la logica para crear productos

--- src/components/productos/AgregarProducto.js
+++ src/components/productos/AgregarProducto.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { Form, Button, Container, Alert } from "react-bootstrap";
 import { campoRequerido, rangoNumero } from "../helpers/helpers";
+import Swal from 'sweetalert2';
+import { useNavigate } from "react-router-dom";
 
-const AgregarProducto = () => {
+const AgregarProducto = (props) => {
   const [nombreProducto, setNombreProducto] = useState("");
   const [precioProducto, setPrecioProducto] = useState(0);
   const [categoria, setCategoria] = useState("");
   const [error, setError] = useState(false);
-
-  const handleSubmit = (e) => {
+  const URL = process.env.REACT_APP_API_URL;
+  const navigation = useNavigate();
+ 
+  const handleSubmit = async(e) => {
     e.preventDefault();
     //validar los datos del form
     if (
@@ -19,6 +23,42 @@ const AgregarProducto = () => {
       //reset del state de error
       setError(false);
       //crear un producto y enviar a la API
+      const productoNuevo = {
+        nombreProducto,
+        precioProducto,
+        categoria
+      }
+      //console.log(productoNuevo);
+      try{
+        const parametros={
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(productoNuevo)
+        }
+        const respuesta = await fetch(URL,parametros)
+        console.log(respuesta);
+        if(respuesta.status === 201){
+          console.log('el producto se creo correctamente')
+          //mostrar cartel al usuario
+          Swal.fire(
+            'Producto creado',
+            'Su producto fue agregado correctamente',
+            'success'
+          )
+          //resetear el formulario
+            e.target.reset();
+          // volver a consultar la api
+            props.consultaAPI();
+          //redireccion a la pagina de lista de productos
+            navigation('/productos');
+        }else{
+          console.log('mostrar un cartel de error')
+        }
+      }catch(error){
+        console.log(error);
+      }
      
     } else {
  
